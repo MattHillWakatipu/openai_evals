ff5bb0f733dc4abf3367d65fe1dac30e2932d238
Matt <50199412+MattHillWakatipu@users.noreply.github.com>
2023-03-29 16:29:01 +1300

    Add functionality to generate ideal answers for regular cards and save the formatted texts to JSONL file

--- data/extractor.py
+++ data/extractor.py
@@ -15,9 +15,9 @@ with open('data.jsonl', 'w') as output_file:
 
             # Get hero data
             name = hero['Name']
-            type_text = hero['Type Text']
             health = hero['Health']
             intelligence = hero['Intelligence']
+            type_text = hero['Type Text']
             functional_text = hero['Functional Text']
 
             # Construct an ideal string from the data
@@ -30,6 +30,29 @@ with open('data.jsonl', 'w') as output_file:
 
     # Open hero file in read mode
     with open('wtr_cards.json', 'r') as card_file:
+
         # Read from JSON file
         cards = json.loads(card_file.read())
-        print(cards)
+
+        # For each card construct an ideal answer string and save it to the JSONL file
+        for card in cards:
+
+            # Get card data
+            name = card['Name']
+            pitch = card['Pitch']
+            cost = card['Cost']
+            power = card['Power']
+            defense = card['Defense']
+            type_text = card['Type Text']
+            functional_text = card['Functional Text']
+
+            # Add the colour for cycle cards
+            # TODO
+
+            # Construct an ideal string from the data
+            ideal = f"{name} is a '{type_text}' card from the 'Welcome to Rathe' set. It costs {cost}, pitches for {pitch}, defends for {defense}, has {power} power, and has the abilities; {functional_text}"
+            json_line = {"input": [{"role": "system", "content": f"In the card game Flesh and Blood, what does the card {name} do?"}], "ideal": ideal}
+
+            # Write to JSONL file
+            output_file.write(json.dumps(json_line))
+            output_file.write('\n')
